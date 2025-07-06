05e7d9bce54fefa3398b323e4077ef21cfa3bbfc
Matheus Kieling <matheus.kieling@serendipe.org>
2025-07-06 10:15:30 -0300

    deleting unecessary truncate sql

--- truncate.sql
+++
@@ -1,8 +0,0 @@
-DO $$ 
-DECLARE r RECORD; 
-BEGIN 
-    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') 
-    LOOP 
-        EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE'; 
-    END LOOP; 
-END $$;
